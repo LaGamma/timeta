59cef3b3c6eccb01a6b36a0d9da3471266b10702
Nicolas LaGamma <nplagamma@email.wm.edu>
2020-09-20 05:17:16 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,22 @@
 $(document).ready(function() {
-	$('#response,#author').fadeOut(400, function (){
-			$.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
-				.done(update)
-				.fail(handleErr);
-			});
-		});
+	$('#hextime,#author').fadeOut(400, function (){})
+		.done(update)
+		.fail(handleErr);
+	});
 
 
-		$('#quote-request').click(function() {
-			$('#response,#author').fadeOut(400, function (){
-				$.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
-				    .done(update)
-				    .fail(handleErr);
-			});		    
-		});
+	$('#time-request').click(function() {
+		$('#hextime').fadeOut(400, function (){})
+			.done(update)
+			.fail(handleErr);		    
+	});
 
-		function update(response) {
-		  //$('#log').prepend('<pre>' + $('#response').html() + '</pre>');
-		  
-		  $('#response').html(response.quoteText);
-		  
+	function update(response) {
+	  //$('#log').prepend('<pre>' + $('#response').html() + '</pre>');
+	  $('#hextime,#author').fadeIn();
+	}
 
-		  if (response.quoteAuthor != "") {
-		  	$('#author').html("- " + response.quoteAuthor);
-		  } else {
-		  	$('#author').html("- unknown");
-		  }
-		  $('#response,#author').fadeIn();
-		}
-
-		function handleErr(jqxhr, textStatus, err) {
-		  console.log("Request Failed: " + textStatus + ", " + err);
-		}
-
-
+	function handleErr(jqxhr, textStatus, err) {
+	  console.log("Request Failed: " + textStatus + ", " + err);
+	}
+});
